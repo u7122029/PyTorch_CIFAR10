26bbb757ac7424c17cb9316bb3ee3305f41bf170
u7122029 <u7122029@anu.edu.au>
2023-04-06 04:34:38 +1000

    Updated pretrained imports for googlenet.

--- cifar10_models/googlenet.py
+++ cifar10_models/googlenet.py
@@ -12,6 +12,10 @@ _GoogLeNetOuputs = namedtuple(
     "GoogLeNetOuputs", ["logits", "aux_logits2", "aux_logits1"]
 )
 
+cifar10_pretrained_weight_urls = {
+    'googlenet': 'https://github.com/u7122029/PyTorch_CIFAR10/releases/download/pretrained/googlenet.pt'
+}
+
 
 def googlenet(pretrained=False, progress=True, device="cpu", **kwargs):
     r"""GoogLeNet (Inception v1) model architecture from
@@ -27,10 +31,7 @@ def googlenet(pretrained=False, progress=True, device="cpu", **kwargs):
     """
     model = GoogLeNet()
     if pretrained:
-        script_dir = os.path.dirname(__file__)
-        state_dict = torch.load(
-            script_dir + "/state_dicts/googlenet.pt", map_location=device
-        )
+        state_dict = torch.hub.load_state_dict_from_url(cifar10_pretrained_weight_urls["googlenet"], map_location=device)
         model.load_state_dict(state_dict)
     return model
 
--- hubconf.py
+++ hubconf.py
@@ -1,5 +1,6 @@
 from cifar10_models.densenet import densenet121, densenet161, densenet169
+from cifar10_models.googlenet import googlenet
 
 if __name__ == "__main__":
-    model = densenet161(pretrained=True)
+    model = googlenet(pretrained=True)
     print(model)
